ed55cdf69590bc41ba1ba4159b220c39a36c1445
Ganesh gowda <gowdaganesh0511@gmail.com>
2025-11-20 19:25:12 +0530

    Creating signin signup Pages and adding rounting

--- packages/zod/src/auth.ts
+++ packages/zod/src/auth.ts
@@ -1,5 +1,7 @@
 import { ParseStatus, z }  from 'zod'
+
 export const signupSchema = z.object({
+    name: z.string(),
     email: z.string().email("Invalid email format"),
     password: z.string().min(6, "Password must be at least 6 characters long"),
     pin: z.string().length(4, "PIN must be exactly 4 digits"),
@@ -9,5 +11,6 @@ export * from "zod";
 
 export const signinSchema = z.object({
     email: z.string().email("Invalid email format"),
-    password: z.string()
+    password: z.string(),
+    pin: z.string()
 })
